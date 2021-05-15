657f2f982e1bbe4f68a2cc238fe6622ae406eb38
michaelo434 <mohsaka@ucsd.edu>
2021-05-14 21:58:48 -0700

    Update script.js
    Cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -6,16 +6,10 @@ const upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
-let user = [];
-let result = [];
-// reset
-let reset = _ => {
-  user = []
-  result = []
-}
 //when generate password button is clicked this runs:
 document.getElementById('generate').addEventListener('click', _ => {
-  reset()
+  let result = [];
+  let password = "";
   // prompt user for valid number of characters for password
   let length = (prompt('Choose the number of characters in your password. \nMinimum: 8 characters\nMaximum: 128 characters')).trim()
   // checks to see if number is valid
@@ -28,44 +22,33 @@ document.getElementById('generate').addEventListener('click', _ => {
     return document.querySelector("textarea").value = ""
   } else {
     // if a proper value was put in, function moves on to confirm lower case letter to add to password
-    let chooseLowCase = confirm('Would you like to add lower case letters? \nClick OK for yes, click CANCEL for no.')
-    if (chooseLowCase === true) {
+    if (confirm('Would you like to add lower case letters? \nClick OK for yes, click CANCEL for no.')) {
       result = result.concat(lowCase)
-    } else {
-      result = result
-      // confirm upper case letters to add
-    } let chooseUpCase = confirm('Would you like to add Upper case letters? \nClick OK for yes, click CANCEL for no.')
-    if (chooseUpCase === true) {
+    }
+    // confirm upper case letters to add
+    if (confirm('Would you like to add Upper case letters? \nClick OK for yes, click CANCEL for no.')) {
       result = result.concat(upCase)
-    } else {
-      result = result
-      //confirm numbers to add
-    } let chooseNum = confirm('Would you like to add numbers? \nClick OK for yes, click CANCEL for no.')
-    if (chooseNum === true) {
+    }
+    //confirm numbers to add
+    if (confirm('Would you like to add numbers? \nClick OK for yes, click CANCEL for no.')) {
       result = result.concat(num);
-    } else {
-      result = result
-      // confirm special numbers to add
-    } let chooseSpecialChar = confirm('Would you like to add special characters? \nClick OK for yes, click CANCEL for no.')
-    if (chooseSpecialChar === true) {
+    }
+    // confirm special numbers to add
+    if (confirm('Would you like to add special characters? \nClick OK for yes, click CANCEL for no.')) {
       result = result.concat(specialChar)
-      // user must choose at least one option or it resets
-    } else if (chooseLowCase === false && chooseUpCase === false && chooseNum === false && chooseSpecialChar === false) {
-      alert(' You must choose at least one of the options above.')
+    }
+    // no available characters
+    if (!result.length) {
+      alert('You must choose at least one of the options above.')
       return document.querySelector("textarea").value = ""
-    } else {
-      result = result
     }
   }
-  // loop to generate all the arrays that were selected to a proper length
-  for (i = 0; i < length; i++) {
-    user.push(result[Math.floor(Math.random() * result.length)]);
+
+  // Generate password string
+  for (let i = 0; i < length; i++) {
+    password = password + result[Math.floor(Math.random() * result.length)];
   }
-  // takes the arrays and turns into string without any "[]" or ","
-  let password = user.join('')
-  console.log(password)
 
   // puts the password onto the "textarea" on the index.html
   document.querySelector("textarea").value = password
 })
-
